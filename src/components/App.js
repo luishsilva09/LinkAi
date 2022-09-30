48509216a7679266489626225193daed89a8c42f
Luís Henrique <luishsilva09@gmail.com>
2022-09-29 23:23:38 -0300

    feat: topbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserContext from "../context/userContext";
+import GlobalStyle from "./globalStyles";
+import TopBar from "./TopBar";
 
 function App() {
   const [userData, setUserData] = React.useState();
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={""} />
-          <Route path="/users/signup" element={""} />
-          <Route path="/users/signin" element={""} />
-          <Route path="/dashboard" element={""} />
-          <Route path="/links/create" element={""} />
-          <Route path="/view/:userId" element={""} />
-        </Routes>
+        <TopBar />
+        <GlobalStyle>
+          <Routes>
+            <Route path="/" element={""} />
+            <Route path="/users/signup" element={""} />
+            <Route path="/users/signin" element={""} />
+            <Route path="/dashboard" element={""} />
+            <Route path="/links/create" element={""} />
+            <Route path="/view/:userId" element={""} />
+          </Routes>
+        </GlobalStyle>
       </BrowserRouter>
     </UserContext.Provider>
   );
---
+++ src/components/TopBar.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "../assets/atom.png";
+import { Link } from "react-router-dom";
+import UserContext from "../context/userContext";
+import { BiExit } from "react-icons/bi";
+
+export default function TopBar() {
+  const { userData } = React.useContext(UserContext);
+  return (
+    <Container>
+      <LeftSide>
+        <img src={Logo} alt="logo" />
+        <h1>LinkAí</h1>
+      </LeftSide>
+      <RightSide>
+        {userData ? (
+          <span>
+            <BiExit size={"30"}></BiExit>
+          </span>
+        ) : (
+          <>
+            <Link to="/">
+              <p>Home</p>
+            </Link>
+            <Link to="/users/signin">
+              <p>Entrar</p>
+            </Link>
+            <Link to="/users/signup">
+              <p>Cadastrar</p>
+            </Link>
+          </>
+        )}
+      </RightSide>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #091d64;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 15px;
+
+  img {
+    height: 55px;
+  }
+  a {
+    color: #fff;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+const LeftSide = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  font-family: "Ubuntu";
+  font-size: 40px;
+`;
+const RightSide = styled.div`
+  display: flex;
+  width: 200px;
+  justify-content: space-between;
+  color: #fff;
+  font-weight: 300;
+  span {
+    width: 100%;
+    text-align: right;
+    cursor: pointer;
+  }
+`;
