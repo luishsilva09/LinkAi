f5438c2a716dc28c4f0a89737637c2214fd33a00
Luís Henrique <luishsilva09@gmail.com>
2022-09-30 00:06:53 -0300

    feat: create home page

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,23 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserContext from "../context/userContext";
 import GlobalStyle from "./globalStyles";
+import Home from "./Home";
 import TopBar from "./TopBar";
 
 function App() {
   const [userData, setUserData] = React.useState();
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
+      <GlobalStyle />
       <BrowserRouter>
-        <TopBar />
-        <GlobalStyle>
-          <Routes>
-            <Route path="/" element={""} />
-            <Route path="/users/signup" element={""} />
-            <Route path="/users/signin" element={""} />
-            <Route path="/dashboard" element={""} />
-            <Route path="/links/create" element={""} />
-            <Route path="/view/:userId" element={""} />
-          </Routes>
-        </GlobalStyle>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/signup" element={""} />
+          <Route path="/users/signin" element={""} />
+          <Route path="/dashboard" element={""} />
+          <Route path="/links/create" element={""} />
+          <Route path="/view/:userId" element={""} />
+        </Routes>
       </BrowserRouter>
     </UserContext.Provider>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import TopBar from "./TopBar";
+
+export default function Home() {
+  console.log("home");
+  return (
+    <>
+      <TopBar />
+      <p>home</p>
+    </>
+  );
+}
