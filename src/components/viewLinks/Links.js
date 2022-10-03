6ea847a769aae4870d85539d7fd802d301f2343b
Luís Henrique <luishsilva09@gmail.com>
2022-10-03 16:57:01 -0300

    feat: page view links

---
+++ src/components/viewLinks/Links.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { ImNewTab } from "react-icons/im";
+export default function Links({ linkData }) {
+  function redirectLink(originalLink) {
+    window.location.assign(originalLink);
+  }
+
+  return (
+    <LinkContainer onClick={() => redirectLink(linkData.originalLink)}>
+      <Left>
+        <Preview src={linkData.previewImage} alt={linkData.tag} />
+        <h2>{linkData.tag}</h2>
+      </Left>
+
+      <ImNewTab size={"30"} />
+    </LinkContainer>
+  );
+}
+
+const LinkContainer = styled.div`
+  width: 100%;
+  min-height: 70px;
+  background-color: #d9d9d9;
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  margin-top: 10px;
+  border-radius: 20px;
+  justify-content: space-between;
+  h2 {
+    font-size: 30px;
+    word-break: break-all;
+  }
+  :hover {
+    cursor: pointer;
+  }
+`;
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Preview = styled.img`
+  height: 60px;
+  width: 60px;
+  margin-right: 15px;
+`;
--- src/components/viewLinks/ViewLinks.js
+++ src/components/viewLinks/ViewLinks.js
@@ -1,5 +1,79 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import TopBar from "../topBar/TopBar";
+import api from "../../services/api";
+import { Oval } from "react-loader-spinner";
+import Links from "./Links";
+
+function contentRender(userData) {
+  return (
+    <Content>
+      <Profile src={userData.imageUrl} alt="perfil" />
+      <p>{userData.name}</p>
+      {userData.links.map((e, index) => (
+        <Links linkData={e} key={index} />
+      ))}
+    </Content>
+  );
+}
 
 export default function ViewLinks() {
-  return <>links</>;
+  const { userId } = useParams();
+  const [userData, setUserData] = useState();
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    getUserData(userId);
+  }, []);
+
+  async function getUserData(userId) {
+    await api.get(`/links/view/${userId}`).then((res) => {
+      setUserData(res.data);
+      setLoad(false);
+    });
+  }
+  return (
+    <Container>
+      {load ? (
+        <Oval color="#091D64" secondaryColor="#094B64" />
+      ) : (
+        contentRender(userData)
+      )}
+    </Container>
+  );
 }
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 540px;
+  margin-top: 20px;
+  @media (max-width: 540px) {
+    width: 100%;
+  }
+
+  p {
+    width: 100%;
+    text-align: center;
+    font-size: 30px;
+  }
+`;
+
+const Profile = styled.img`
+  height: 260px;
+  width: 260px;
+  object-fit: cover;
+  border-radius: 50%;
+  @media (max-width: 540px) {
+    width: 160px;
+    height: 160px;
+  }
+`;
