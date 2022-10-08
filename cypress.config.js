bed5a3bc8777bfb656cdc54f8e55f573dfe0ee3b
Luís Henrique <luishsilva09@gmail.com>
2022-10-08 09:53:05 -0300

    feat: config env file

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      API_URL: "http://localhost:4000",
+      FRONT_URL: "http://localhost:3000",
+    },
   },
 });
