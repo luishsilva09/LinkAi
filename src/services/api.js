952a155c44c9d12f9d2f7c13118fdb5498e34422
Luís Henrique <luishsilva09@gmail.com>
2022-10-09 18:57:38 -0300

    fix: url from api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
-console.log("process", process.env.REACT_APP_BASE_URL);
 export default instance;
